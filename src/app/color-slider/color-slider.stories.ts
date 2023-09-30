a6436f4aef3f3508619dea4428d42dc8807cd1f6
John R <24492257+pawn002@users.noreply.github.com>
2023-09-30 06:12:52 -0400

    chore: initial layout

--- src/app/color-slider/color-slider.stories.ts
+++ src/app/color-slider/color-slider.stories.ts
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/angular';
-// import Button from './button.component';
 import { ColorSliderComponent } from './color-slider.component';
 
 // More on how to set up stories at: https://storybook.js.org/docs/angular/writing-stories/introduction
@@ -9,12 +8,12 @@ const meta: Meta<ColorSliderComponent> = {
   tags: ['autodocs'],
   render: (args: ColorSliderComponent) => ({
     props: {
-      backgroundColor: null,
+      color: null,
       ...args,
     },
   }),
   argTypes: {
-    backgroundColor: {
+    color: {
       control: 'color',
     },
   },
@@ -24,29 +23,6 @@ export default meta;
 type Story = StoryObj<ColorSliderComponent>;
 
 // More on writing stories with args: https://storybook.js.org/docs/angular/writing-stories/args
-export const Primary: Story = {
-  args: {
-    primary: true,
-    label: 'Button',
-  },
-};
-
-export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
+export const Typical: Story = {
+  args: {},
 };
