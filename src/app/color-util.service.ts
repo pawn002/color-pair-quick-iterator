b781ff878231a40ec62457b0b321ce2d07cce4a1
John R <24492257+pawn002@users.noreply.github.com>
2023-10-02 06:05:33 -0400

    feat: created color parsing function - handles error so that it doesnt crash app

--- src/app/color-util.service.ts
+++ src/app/color-util.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import Color from 'colorjs.io';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ColorUtilService {
+  parseColor(color: string) {
+    let parsedColor: Object | null = null;
 
-  constructor() { }
+    try {
+      parsedColor = Color.parse(color);
+    } catch (error) {
+      console.error(error);
+    }
+    return parsedColor;
+  }
+
+  generateAllLightnessCandidatesWithConstantChroma(color: string) {
+    const parsedColor = this.parseColor(color);
+
+    console.log(parsedColor);
+  }
+
+  constructor() {}
 }
