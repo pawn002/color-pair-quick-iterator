48674187ca4709665ee5c66d3ff3a5ef159fd87e
John R <24492257+pawn002@users.noreply.github.com>
2023-10-08 06:21:32 -0400

    chore: initial creation of comp

---
+++ src/app/color-contrast/color-contrast.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-color-contrast',
+  templateUrl: './color-contrast.component.html',
+  styleUrls: ['./color-contrast.component.scss']
+})
+export class ColorContrastComponent {
+
+}
