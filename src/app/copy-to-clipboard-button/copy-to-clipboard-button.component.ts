dd4c70fab9e037ed291334dae90a098ac1099df7
John R <24492257+pawn002@users.noreply.github.com>
2023-10-09 21:32:40 -0400

    chore: initial comp ready

--- src/app/copy-to-clipboard-button/copy-to-clipboard-button.component.ts
+++ src/app/copy-to-clipboard-button/copy-to-clipboard-button.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-copy-to-clipboard-button',
   templateUrl: './copy-to-clipboard-button.component.html',
-  styleUrls: ['./copy-to-clipboard-button.component.scss']
+  styleUrls: ['./copy-to-clipboard-button.component.scss'],
 })
 export class CopyToClipboardButtonComponent {
+  @Input() color: string | null = null;
 
+  async copyToClipboard(): Promise<void> {
+    if (this.color) {
+      try {
+        await navigator.clipboard.writeText(this.color);
+
+        console.log(this.color, 'copied to clipboard');
+      } catch (err) {
+        console.error('Failed to copy text: ', err);
+      }
+    } else {
+      console.error('nothing to copy');
+    }
+  }
 }
