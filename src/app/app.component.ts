67e59c9a785cc2deb60b5f421c53312c7dd9273c
John R <24492257+pawn002@users.noreply.github.com>
2023-10-10 10:27:09 -0400

    chore: wcag toggle working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ContrastType } from './services/color-metrics.service';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,17 @@ export class AppComponent {
 
   colorPickerTwoSelectedColor: string | null = null;
   colorPickerTwoComparedColor: string | null = null;
+
+  contrastType: ContrastType = 'apca';
+
+  toggleContrastType(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const checked = inputElement.checked;
+
+    console.log(`toggling contrast type ${inputElement.checked}`);
+
+    this.contrastType = checked ? 'bpca' : 'apca';
+  }
 
   handleColorInputInput(inputNumber: 'One' | 'Two', event: string) {
     if (inputNumber === 'One') {
