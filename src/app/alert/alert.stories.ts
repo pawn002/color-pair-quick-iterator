b27e857a51da56f7cf1e1804813d59a95747dff3
John R <24492257+pawn002@users.noreply.github.com>
2023-10-14 10:36:53 -0400

    chore: created story file

---
+++ src/app/alert/alert.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { AlertComponent } from './alert.component';
+
+// More on how to set up stories at: https://storybook.js.org/docs/angular/writing-stories/introduction
+const meta: Meta<AlertComponent> = {
+  title: 'App/Alert',
+  component: AlertComponent,
+  tags: ['autodocs'],
+  render: (args: AlertComponent) => ({
+    props: {
+      ...args,
+    },
+  }),
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<AlertComponent>;
+
+// More on writing stories with args: https://storybook.js.org/docs/angular/writing-stories/args
+export const Typical: Story = {
+  args: {},
+};
