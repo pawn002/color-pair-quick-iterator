7c3a1983fc9160c37918e4050e62559b66396a2f
John R <24492257+pawn002@users.noreply.github.com>
2023-10-26 22:46:56 -0400

    chore: implemented bpca algo - had to copy over code because node module does not work as per documentation

--- src/app/services/color-metrics.service.ts
+++ src/app/services/color-metrics.service.ts
@@ -5,6 +5,7 @@ import { calcAPCA, sRGBtoY } from 'apca-w3';
 import { scaleLinear } from 'd3';
 
 import { ColorUtilService } from './color-util.service';
+import { BpcaService } from './bpca.service';
 
 export interface NumberKeyLookup {
   [key: number]: number;
@@ -37,23 +38,9 @@ export class ColorMetricsService {
       }
 
       if (contrastType === 'bpca') {
-        const parsedTripleE = this.cus.parseColor('#eeeeee')?.coords;
-        const parsedColorOne = this.cus.parseColor(colorOne)?.coords;
-        const parsedColorTwo = this.cus.parseColor(colorTwo)?.coords;
+        const bpcaScore = this.calcRawBpcaContrast(colorOne, colorTwo);
 
-        const trippleEY = sRGBtoY(parsedTripleE);
-        const colorOneY = sRGBtoY(parsedColorOne);
-        const colorTwoY = sRGBtoY(parsedColorTwo);
-
-        let wcagStyleScore: number;
-
-        if (colorOneY > trippleEY && colorOneY > colorTwoY && this.dev) {
-          wcagStyleScore = this.transformAPCAToWCAGAlt(contrast);
-        } else {
-          wcagStyleScore = this.transformAPCAToWCAG(contrast);
-        }
-
-        score = wcagStyleScore;
+        score = bpcaScore;
       }
     } else {
       console.error(`Raw APCA contrast was not calculable`);
@@ -70,83 +57,18 @@ export class ColorMetricsService {
     return score;
   }
 
-  transformAPCAToWCAG(apcaScore: number): number {
-    let wcag: number = NaN;
-
-    // [Source for numbers](https://github.com/Myndex/bridge-pca#additional-notes)
-    const scaleApcaToWcag = scaleLinear(
-      // WCAG-ish Range
-      [1, 3, 4.5, 7, 21]
-    ).domain(
-      // APCA Absolute Range
-      [0, 60, 75, 90, 108]
+  calcRawBpcaContrast(colorOne: string, colorTwo: string) {
+    const lc = this.bpca.calcBPCA(colorOne, colorTwo) as number;
+    const colorOneArray255 = this.cus.getRgb255Array(colorOne) as number[];
+    const colorTwoArray255 = this.cus.getRgb255Array(colorTwo) as number[];
+    const colorOneY = this.bpca.sRGBtoY(colorOneArray255);
+    const colorTwoY = this.bpca.sRGBtoY(colorTwoArray255);
+    const wcag = parseFloat(
+      this.bpca.bridgeRatio(lc, colorOneY, colorTwoY, '')
     );
-
-    const absoluteApca: number = Math.abs(apcaScore);
-
-    if (this.apcaToWcagLookup[absoluteApca]) {
-      // use memoized value if present
-      wcag = this.apcaToWcagLookup[absoluteApca];
-    } else {
-      const wcagStyleScore = scaleApcaToWcag(absoluteApca);
-
-      let roundedWcag = parseFloat(wcagStyleScore.toFixed(1));
-
-      if (roundedWcag > 21) {
-        roundedWcag = 21;
-      }
-
-      // memoize score for later access
-      this.apcaToWcagLookup[absoluteApca] = roundedWcag;
-
-      wcag = roundedWcag;
-    }
 
     return wcag;
   }
 
-  // TODO: create alt form of `transformAPCAToWCAG` to handle special case described in #37
-  transformAPCAToWCAGAlt(apcaScore: number): number {
-    let wcag: number = NaN;
-
-    // [Source for numbers](https://github.com/Myndex/bridge-pca#additional-notes)
-    const scaleApcaToWcag = scaleLinear(
-      // WCAG-ish Range
-      [1, 3, 4.5, 7, 21]
-    ).domain(
-      // APCA Absolute Range
-      [0, 70, 85, 100, 108]
-    );
-
-    const absoluteApca: number = Math.abs(
-      // Subtracting one from APCA raw score to ensure no false passes going from APCA to WCAG Style.
-      apcaScore
-    );
-
-    if (this.apcaToWcagLookupAlt[absoluteApca]) {
-      // use memoized value if present
-      wcag = this.apcaToWcagLookupAlt[absoluteApca];
-    } else {
-      const wcagStyleScore = scaleApcaToWcag(absoluteApca);
-
-      let roundedWcag = parseFloat(wcagStyleScore.toFixed(1));
-
-      if (roundedWcag > 21) {
-        roundedWcag = 21;
-      }
-
-      // memoize score for later access
-      this.apcaToWcagLookupAlt[absoluteApca] = roundedWcag;
-
-      wcag = roundedWcag;
-    }
-
-    return wcag;
-  }
-
-  // TODO: create modified version of [Bridge-PCA algo](https://github.com/Myndex/bridge-pca/blob/master/src/bridge-pca.js)
-  // Likely need a new service. . .
-  calcRawBpcaContrast(colorOne: string, colorTwo: string) {}
-
-  constructor(private cus: ColorUtilService) {}
+  constructor(private cus: ColorUtilService, private bpca: BpcaService) {}
 }
