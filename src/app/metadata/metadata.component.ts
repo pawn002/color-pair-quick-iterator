f532dbe1289975cf18544c1452d5bb6722c97a0c
John R <24492257+pawn002@users.noreply.github.com>
2023-10-27 15:40:28 -0400

    chore: added additional differences to meta

--- src/app/metadata/metadata.component.ts
+++ src/app/metadata/metadata.component.ts
@@ -1,8 +1,12 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { ColorUtilService, ColorMetaObj } from '../services/color-util.service';
+import { ColorMetricsService } from '../services/color-metrics.service';
 
 export class DifferencesDataObj {
   deltaE: number | null = null;
+  wcag2Old: number | null = null;
+  wcag2New: number | null = null;
+  apca: number | null = null;
 }
 
 @Component({
@@ -16,19 +20,42 @@ export class MetadataComponent implements OnChanges {
 
   differences: DifferencesDataObj = {
     deltaE: null,
+    wcag2Old: null,
+    wcag2New: null,
+    apca: null,
   };
 
   colorOneMeta: ColorMetaObj | null = null;
 
   colorTwoMeta: ColorMetaObj | null = null;
 
-  constructor(private cus: ColorUtilService) {}
+  constructor(
+    private cus: ColorUtilService,
+    private cms: ColorMetricsService
+  ) {}
 
   ngOnChanges() {
     if (this.colorOne && this.colorTwo) {
       this.differences.deltaE = this.cus.calcDeltaE(
         this.colorOne,
         this.colorTwo
+      );
+
+      this.differences.wcag2New = this.cms.getContrast(
+        this.colorOne,
+        this.colorTwo,
+        'bpca'
+      );
+
+      this.differences.wcag2Old = this.cus.calcWcag2(
+        this.colorOne,
+        this.colorTwo
+      );
+
+      this.differences.apca = this.cms.getContrast(
+        this.colorOne,
+        this.colorTwo,
+        'apca'
       );
 
       if (this.cus.getColorMeta(this.colorOne)) {
