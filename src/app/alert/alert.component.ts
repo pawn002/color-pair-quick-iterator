4b5325e537444e302beabc221b3f68a97592d780
John R <24492257+pawn002@users.noreply.github.com>
2023-11-03 05:12:08 -0400

    chore: code cleanup of alert feature

--- src/app/alert/alert.component.ts
+++ src/app/alert/alert.component.ts
@@ -1,18 +1,42 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  OnChanges,
+  SimpleChanges,
+  OnInit,
+} from '@angular/core';
+import { random } from 'lodash';
+
+export interface AlertMessagObj {
+  message: string;
+}
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.scss'],
 })
-export class AlertComponent {
-  @Input() showAlert: boolean = false;
-  @Input() alertMessage: string | null = null;
+export class AlertComponent implements OnInit, OnChanges {
+  @Input() alertMessage: AlertMessagObj | null = null;
   @Output() alertClosed = new EventEmitter<boolean>();
+
+  showAlert: boolean = false;
 
   hideAlert() {
     this.showAlert = false;
 
     this.alertClosed.emit(true);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(`new obj!`);
+
+    this.showAlert = true;
+  }
+
+  ngOnInit(): void {
+    this.showAlert = false;
   }
 }
