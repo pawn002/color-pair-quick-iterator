c6f58c4c858ecfaca64c417ec1017efc44a43750
John R <24492257+pawn002@users.noreply.github.com>
2023-11-05 23:05:17 -0500

    chore: created typing for table data

--- src/app/palette-table/palette-table.component.ts
+++ src/app/palette-table/palette-table.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 
+export class TableColorCell {
+  color: string | null = null;
+  lightness: number = NaN;
+  chroma: number = NaN;
+  pContrast: number = NaN;
+  wacg2Comp: number = NaN;
+}
+
+export type TableRow = Array<TableColorCell>;
+
+export type TableData = Array<TableRow>;
+
 @Component({
   selector: 'app-palette-table',
   templateUrl: './palette-table.component.html',
@@ -9,7 +21,7 @@ export class PaletteTableComponent implements OnInit {
   tableHeaders: Array<number> = [];
 
   // An array of arrays where each array is a 'row' of data, and objects are cells of data.
-  dataStruct = [
+  dataStruct: TableData = [
     [
       {
         color: 'white',
