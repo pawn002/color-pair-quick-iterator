f712e9025d66483471641749e0cfa5e3b0b144ea
John R <24492257+pawn002@users.noreply.github.com>
2023-11-05 22:01:55 -0500

    chore: initial data and template planning

--- src/app/palette-table/palette-table.component.ts
+++ src/app/palette-table/palette-table.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-palette-table',
   templateUrl: './palette-table.component.html',
-  styleUrls: ['./palette-table.component.scss']
+  styleUrls: ['./palette-table.component.scss'],
 })
 export class PaletteTableComponent {
-
+  // An array of arrays where each array is a 'row' of data, and objects are cells of data.
+  dataStruct = [
+    [
+      {
+        color: '',
+        lightness: NaN,
+        Chroma: NaN,
+        pContrast: NaN,
+        wacg2Comp: NaN,
+      },
+    ],
+  ];
 }
