fac5c8d478944d2e90260ecf63a1d3fc9990cafb
John R <24492257+pawn002@users.noreply.github.com>
2023-11-05 21:20:14 -0500

    chore: init comp files

---
+++ src/app/palette-table/palette-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-palette-table',
+  templateUrl: './palette-table.component.html',
+  styleUrls: ['./palette-table.component.scss']
+})
+export class PaletteTableComponent {
+
+}
