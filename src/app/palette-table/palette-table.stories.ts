bd55a58a0e964568c2716416674d75b6e40124d3
John R <24492257+pawn002@users.noreply.github.com>
2023-11-05 21:24:53 -0500

    chore: added story file

---
+++ src/app/palette-table/palette-table.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { PaletteTableComponent } from './palette-table.component';
+
+// More on how to set up stories at: https://storybook.js.org/docs/angular/writing-stories/introduction
+const meta: Meta<PaletteTableComponent> = {
+  title: 'App/Palette Table',
+  component: PaletteTableComponent,
+  tags: ['autodocs'],
+  render: (args: PaletteTableComponent) => ({
+    props: {
+      ...args,
+    },
+  }),
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<PaletteTableComponent>;
+
+// More on writing stories with args: https://storybook.js.org/docs/angular/writing-stories/args
+export const Typical: Story = {
+  args: {},
+};
