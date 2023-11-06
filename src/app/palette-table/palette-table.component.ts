6ab9c82564d8d0bfecb73cc36d2f1385f9806d22
John R <24492257+pawn002@users.noreply.github.com>
2023-11-05 23:00:27 -0500

    chore: created function that gets table headers

--- src/app/palette-table/palette-table.component.ts
+++ src/app/palette-table/palette-table.component.ts
@@ -1,32 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-palette-table',
   templateUrl: './palette-table.component.html',
   styleUrls: ['./palette-table.component.scss'],
 })
-export class PaletteTableComponent {
+export class PaletteTableComponent implements OnInit {
+  tableHeaders: Array<number> = [];
+
   // An array of arrays where each array is a 'row' of data, and objects are cells of data.
   dataStruct = [
     [
       {
         color: 'white',
         lightness: 1,
-        Chroma: 0,
+        chroma: 0,
         pContrast: NaN,
         wacg2Comp: NaN,
       },
       {
         color: null,
         lightness: 1,
-        Chroma: 0.2,
+        chroma: 0.2,
         pContrast: NaN,
         wacg2Comp: NaN,
       },
       {
         color: null,
         lightness: 1,
-        Chroma: 0.4,
+        chroma: 0.4,
         pContrast: NaN,
         wacg2Comp: NaN,
       },
@@ -35,21 +37,21 @@ export class PaletteTableComponent {
       {
         color: 'grey',
         lightness: 0.5,
-        Chroma: 0,
+        chroma: 0,
         pContrast: NaN,
         wacg2Comp: NaN,
       },
       {
         color: '#2456d3',
         lightness: 0.5,
-        Chroma: 0.2,
+        chroma: 0.2,
         pContrast: NaN,
         wacg2Comp: NaN,
       },
       {
         color: '#0137f6',
         lightness: 0.5,
-        Chroma: 0.4,
+        chroma: 0.4,
         pContrast: NaN,
         wacg2Comp: NaN,
       },
@@ -58,27 +60,41 @@ export class PaletteTableComponent {
       {
         color: 'black',
         lightness: 0,
-        Chroma: 0,
+        chroma: 0,
         pContrast: NaN,
         wacg2Comp: NaN,
       },
       {
         color: null,
         lightness: 0,
-        Chroma: 0.2,
+        chroma: 0.2,
         pContrast: NaN,
         wacg2Comp: NaN,
       },
       {
         color: null,
         lightness: 0,
-        Chroma: 0.4,
+        chroma: 0.4,
         pContrast: NaN,
         wacg2Comp: NaN,
       },
     ],
   ];
 
-  // TODO: create function that creates column headers for table based on dynamic data
-  getTableColumnHeaders() {}
+  getTableColumnHeaders() {
+    const headers = [];
+
+    const sampleRow = this.dataStruct[0];
+    for (let i = 0; i < sampleRow.length; i++) {
+      const curCell = sampleRow[i];
+
+      headers.push(curCell.chroma);
+    }
+
+    this.tableHeaders = headers;
+  }
+
+  ngOnInit(): void {
+    this.getTableColumnHeaders();
+  }
 }
